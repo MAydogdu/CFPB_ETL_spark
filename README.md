f845c1cde436754dafb46113e1ea42446e0915e3
Murat Aydogdu <murt001@gmail.com>
2018-04-25 16:44:41 -0400

    First commit
    The python program, supporting data files and a descriptive PDF file are all included.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CFPB_ETL_spark
+The pdf file
+CFPB_AWS_2018April.pdf
+has the information regarding this project. I ran the python notebook on an AWS spark cluster.
+The dataset is not too huge: just over 500MB.
+The goal is really to practice parallel processing concepts and spark.
---
+++ uploadtocluster.sh
@@ -0,0 +1,5 @@
+scp -i "user001.pem" CFPB_Complaints_sparkETL_2018April.ipynb  hadoop@ec2-18-219-168-68.us-east-2.compute.amazonaws.com:~/cons
+scp -i "user001.pem" Issues_recoded.csv  hadoop@ec2-18-219-168-68.us-east-2.compute.amazonaws.com:~/cons
+scp -i "user001.pem" Products_recoded.csv  hadoop@ec2-18-219-168-68.us-east-2.compute.amazonaws.com:~/cons
+scp -i "user001.pem" LoughranMcDonald_Negative.csv  hadoop@ec2-18-219-168-68.us-east-2.compute.amazonaws.com:~/cons
+scp -i "user001.pem" LoughranMcDonald_Positive.csv  hadoop@ec2-18-219-168-68.us-east-2.compute.amazonaws.com:~/cons
